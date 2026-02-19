3ccf2a5a66db4689e9528629920d5bf978bb38d1
Sagar Patel <sp@4fo.de>
2026-02-19 12:01:25 +0530

    Update the design of hero section in home page

--- src/app/basic-page/home/home.component.ts
+++ src/app/basic-page/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AfterViewInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
@@ -8,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('heroCard', { static: false }) heroCardRef!: ElementRef;
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {}
+
+  onHeroMouseMove(event: MouseEvent) {
+    const card = this.heroCardRef?.nativeElement;
+    if (!card) return;
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = ((y - centerY) / centerY) * 10; // max 10deg
+    const rotateY = ((x - centerX) / centerX) * -10;
+    this.renderer.setStyle(card, 'transform', `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
+    this.renderer.addClass(card, 'is-tilting');
+  }
+
+  onHeroMouseLeave() {
+    const card = this.heroCardRef?.nativeElement;
+    if (!card) return;
+    this.renderer.setStyle(card, 'transform', 'rotateX(0deg) rotateY(0deg)');
+    this.renderer.removeClass(card, 'is-tilting');
+  }
 
 }
