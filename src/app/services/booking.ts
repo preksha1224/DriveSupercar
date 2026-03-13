953d746fc9506a2bbc418318b5d38a7fb56bfdb5
Preksha Patil <pp@4fo.de>
2026-03-13 11:23:16 +0530

    booking-service

---
+++ src/app/services/booking.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Booking {
+  constructor(private http: HttpClient){}
+
+  createBooking(req: any) : Observable<any>{
+    return this.http.post('/book/createBooking',req);
+  }
+
+  getBooking() : Observable<any>{
+    return this.http.get('/book/getBookings')
+  }
+}
